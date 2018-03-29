e24394f4164afa6114cb923548b4fbb360ce51af
Ian Pollack <ianjpollack@gmail.com>
2018-03-29 18:39:36 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,9 @@ require "pry"
 def reformat_languages(languages)
   newHash = {}
   languages.each do |style, style_data|
+    style_data.each do |language, language_data|
     binding.pry
+  end
   end
 
 
